6b3ab2e868b15f1ff50ec1e90d2924fd96cdadc0
Nijiko Yonskai <nijikokun@gmail.com>
2013-04-02 15:30:01 -0700

    Here's the money shot

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = exports = {
+  environment: {
+    type: 'development',
+    level: 'info'
+  },
+  port: 3000
+};
---
+++ server/index.js
@@ -0,0 +1,31 @@
+var Express = require('express'),
+    Log = require('log'),
+    Moth = require('mashape-oauth'),
+    OAuth = Moth.OAuth,
+    OAuth2 = Moth.OAuth2;
+
+var config = require ('./config.js'),
+    log = new Log(config.environment.level);
+
+var app = Express();
+
+app.config(function () {
+  app.set('view engine', 'ejs');
+  app.set('views', __dirname + '/_views');
+  app.use(express.static(__dirname + '/_assets'));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser('maeby, lets keep it a secret?'));
+  app.use(express.session());
+});
+
+app.get('/oauth/:version/:leg/:action', function (req, res) {
+  var version = req.params.version,
+      leg = req.params.leg,
+      action = req.params.action;
+
+  log.info('Version: ' + version);
+  log.info('Leg: ' + leg);
+  log.info('Action: ' + action);
+});
+
+app.listen(config.port);
