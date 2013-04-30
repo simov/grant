e7a3134b8c75169d6cfbbe1da20836f9bb80cc58
Nijiko Yonskai <nijikokun@gmail.com>
2013-04-29 17:16:24 -0700

    Wrote this with a basic text editor on a whim, fixed in sublimetext

--- tests/factual.js
+++ tests/factual.js
@@ -1,61 +1,61 @@
 var request = require('request'),
-	query = require('querystring'),
-    args    = require('optimist').options('key').options('secret').argv;
+  query = require('querystring'),
+  args = require('optimist').options('key').options('secret').argv;
 
 var options = {
-	factual: {
-		url: 'http://api.v3.factual.com/t/places'
-	}
+  factual: {
+    url: 'http://api.v3.factual.com/t/places'
+  }
 };
 
 if (!args.key) 
-	throw new Error('Missing Consumer Key! -key cli option');
+  throw new Error('Missing Consumer Key! -key cli option');
 
 if (!args.secret) 
-	throw new Error('Missing Consumer Secret! -secret cli option');
+  throw new Error('Missing Consumer Secret! -secret cli option');
 
 request({
-	method: 'POST',
-	uri: 'http://localhost:3000/store',
-	form: {
-		consumer_key: args.key,
-		consumer_secret: args.secret,
-		signature_method: "HMAC-SHA1",
+  method: 'POST',
+  uri: 'http://localhost:3000/store',
+  form: {
+    consumer_key: args.key,
+    consumer_secret: args.secret,
+    signature_method: "HMAC-SHA1",
 
-		auth_type: "oauth",
-		auth_version: 1,
-		auth_leg: 1,
+    auth_type: "oauth",
+    auth_version: 1,
+    auth_leg: 1,
 
-		callback: "oob",
-		version: "1.0"
-	}
+    callback: "oob",
+    version: "1.0"
+  }
 }, function (error, response, body) {
-	if (error) return console.log(error);
-	body = JSON.parse(body);
+  if (error) return console.log(error);
+  body = JSON.parse(body);
 
-	request({
-		method: 'POST',
-		uri: 'http://localhost:3000/start',
-		form: {
-			hash: body.hash,
+  request({
+    method: 'POST',
+    uri: 'http://localhost:3000/start',
+    form: {
+      hash: body.hash,
 
-			// Request Details
-			method: 'GET',
-			url: options.factual.url
-		},
-		followAllRedirects: true
-	}, function (error, response, body) {
-		var auth = JSON.parse(body).authorization;
+      // Request Details
+      method: 'GET',
+      url: options.factual.url
+    },
+    followAllRedirects: true
+  }, function (error, response, body) {
+    var auth = JSON.parse(body).authorization;
 
-		var req = request({
-			method: 'GET',
-			uri: options.factual.url,
-			headers: {
-				"Authorization": auth
-			}
-		}, function (error, response, body) {
-			console.log(body);
-		});
-		console.log(req.headers);
-	});
+    var req = request({
+      method: 'GET',
+      uri: options.factual.url,
+      headers: {
+        "Authorization": auth
+      }
+    }, function (error, response, body) {
+      console.log(body);
+    });
+    console.log(req.headers);
+  });
 });
