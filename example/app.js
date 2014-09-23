f2896abc688973dfcaa51592531309e6f0c55ec8
simov <simeonvelichkov@gmail.com>
2014-09-23 22:15:49 +0300

    Add tumblr

--- example/app.js
+++ example/app.js
@@ -70,7 +70,7 @@ app.get('/', function (req, res) {
     'bitly', 'github', 'stackexchange', 'google', 'yahoo',
     'foursquare', 'slack', 'instagram', 'flickr', 'trello',
     'asana', 'mailchimp', 'heroku', 'dropbox', 'openstreetmap',
-    'box', 'stripe'
+    'box', 'stripe', 'tumblr'
 
     // 'disqus'
   ];
