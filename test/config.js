99cc6dac8ca4204c00ecc8e5333a125ff2ed0b9e
simov <simeonvelichkov@gmail.com>
2014-10-21 17:24:26 +0300

    New data structure supporting provider overrides

--- test/config.js
+++ test/config.js
@@ -4,73 +4,133 @@ var Grant = require('../guardian');
 
 
 describe('options', function () {
-  it('set oauth1 credentials', function () {
-    var config = {
-      credentials: {twitter:{key:'key', secret:'secret'}}
-    };
-    (new Grant(config));
-    config.app.twitter.consumer_key.should.equal('key');
-    config.app.twitter.consumer_secret.should.equal('secret');
+
+  describe('shortcuts', function () {
+    it('set provider shortcuts', function () {
+      var config = {
+        server: {},
+        twitter:{}
+      };
+      (new Grant(config));
+      config.app.twitter.twitter.should.equal(true);
+      config.app.twitter.name.should.equal('twitter');
+    });
   });
-  it('set oauth2 credentials', function () {
-    var config = {
-      credentials: {facebook:{key:'key', secret:'secret'}}
-    };
-    (new Grant(config));
-    config.app.facebook.client_id.should.equal('key');
-    config.app.facebook.client_secret.should.equal('secret');
+
+  describe('credentials', function () {
+    it('set OAuth1 credentials', function () {
+      var config = {
+        server: {},
+        twitter:{key:'key', secret:'secret'}
+      };
+      (new Grant(config));
+      config.app.twitter.consumer_key.should.equal('key');
+      config.app.twitter.consumer_secret.should.equal('secret');
+    });
+    it('set OAuth2 credentials', function () {
+      var config = {
+        server:{},
+        facebook:{key:'key', secret:'secret'}
+      };
+      (new Grant(config));
+      config.app.facebook.client_id.should.equal('key');
+      config.app.facebook.client_secret.should.equal('secret');
+    });
   });
-  it('copy all options', function () {
-    var config = {
-      options: {github:{headers:{'User-Agent':'Grant'}, access_token:'token'}}
-    };
-    (new Grant(config));
-    should.deepEqual(config.app.github.headers, {'User-Agent':'Grant'});
-    config.app.github.access_token.should.equal('token');
+
+  describe('scope', function () {
+    it('join scopes with comma', function () {
+      var config = {
+        server:{},
+        facebook:{scope:['scope1','scope2']}
+      };
+      (new Grant(config));
+      config.app.facebook.scope.should.equal('scope1,scope2');
+    });
+    it('join scopes with space', function () {
+      var config = {
+        server:{},
+        google:{scope:['scope1','scope2']}
+      };
+      (new Grant(config));
+      config.app.google.scope.should.equal('scope1 scope2');
+    });
+    it('set google offline scope outside of the regular scopes', function () {
+      var config = {
+        server:{},
+        google:{scope:['scope1','offline']}
+      };
+      (new Grant(config));
+      config.app.google.scope.should.equal('scope1');
+      config.app.google.access_type.should.equal('offline');
+    });
+    it('set linkedin scopes as querystring', function () {
+      var config = {
+        server:{},
+        linkedin:{scope:['scope1','scope2']}
+      };
+      (new Grant(config));
+      config.app.linkedin.request_url.should.equal(
+        'https://api.linkedin.com/uas/oauth/requestToken?scope=scope1,scope2');
+    });
   });
-  it('use server final callback', function () {
-    var config = {
-      server: {callback:'/callback'},
-      options: {facebook:{}}
-    };
-    (new Grant(config));
-    config.app.facebook.callback.should.equal('/callback');
+
+  describe('server', function () {
+    it('set server options', function () {
+      var config = {
+        server: {protocol:'http', host:'localhost:3000', callback:'/'},
+        facebook:{}
+      };
+      (new Grant(config));
+      config.app.facebook.protocol.should.equal('http');
+      config.app.facebook.host.should.equal('localhost:3000');
+      config.app.facebook.callback.should.equal('/');
+    });
+    it('override server options', function () {
+      var config = {
+        server: {protocol:'http', host:'localhost:3000', callback:'/'},
+        facebook:{protocol:'https', host:'dummy.com:3000', callback:'/callback'}
+      };
+      (new Grant(config));
+      config.app.facebook.protocol.should.equal('https');
+      config.app.facebook.host.should.equal('dummy.com:3000');
+      config.app.facebook.callback.should.equal('/callback');
+    });
   });
-  it('set specific final callback', function () {
-    var config = {
-      server: {callback:'/callback'},
-      options: {facebook:{callback:'/facebook/callback'}}
-    };
-    (new Grant(config));
-    config.app.facebook.callback.should.equal('/facebook/callback');
+
+  describe('misc', function () {
+    it('set OAuth2 state', function () {
+      var config = {
+        server:{},
+        facebook:{state:'Grant'}
+      };
+      (new Grant(config));
+      config.app.facebook.state.should.equal('Grant');
+    });
+    it('set headers', function () {
+      var config = {
+        server:{},
+        facebook:{}
+      };
+      (new Grant(config));
+      should.deepEqual(config.app.facebook.headers, {'User-Agent':'Grant'});
+    });
   });
-  it('set scope as string', function () {
-    var config = {
-      options: {facebook:{scope:'scope1,scope2'}}
-    };
-    (new Grant(config));
-    config.app.facebook.scope.should.equal('scope1,scope2');
-  });
-  it('set scope as array', function () {
-    var config = {
-      options: {facebook:{scope:['scope1','scope2']}}
-    };
-    (new Grant(config));
-    config.app.facebook.scope.should.equal('scope1,scope2');
-  });
-  it('set scope as array - google', function () {
-    var config = {
-      options: {google:{scope:['scope1','scope2']}}
-    };
-    (new Grant(config));
-    config.app.google.scope.should.equal('scope1 scope2');
-  });
-  it('set linkedin scope as a querystring', function () {
-    var config = {
-      options: {linkedin:{scope:['scope1','scope2']}}
-    };
-    (new Grant(config));
-    config.app.linkedin.request_url.should.equal(
-      'https://api.linkedin.com/uas/oauth/requestToken?scope=scope1,scope2');
+
+  describe('override', function () {
+    it('override provider options', function () {
+      var config = {
+        server:{host:'dummy.com:3000'},
+        facebook:{
+          scope:['scope1'],
+          custom:{scope:['scope2']}
+        }
+      };
+      (new Grant(config));
+      config.app.facebook.host.should.equal('dummy.com:3000');
+      config.app.facebook.scope.should.equal('scope1');
+      config.app.facebook.overrides.custom.host.should.equal('dummy.com:3000');
+      config.app.facebook.overrides.custom.scope.should.equal('scope2');
+    });
   });
 });
