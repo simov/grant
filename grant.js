d0accfdaab8df6b554d3d2fdca65e9946fad6611
simov <simeonvelichkov@gmail.com>
2014-12-18 11:35:15 +0200

    Move the flow logic outside of the route callbacks

--- grant.js
+++ grant.js
@@ -10,6 +10,11 @@ var express = require('express'),
 
 var request = require('request');
 var config = require('./config');
+var flows = {
+  1: require('./lib/oauth1'),
+  2: require('./lib/oauth2'),
+  getpocket: require('./lib/getpocket')
+}
 
 
 function Grant (_config) {
@@ -79,181 +84,77 @@ function Grant (_config) {
       client_id, client_secret, consumer_key, consumer_secret
     */
 
-    var redirect_uri = provider.protocol + '://' + provider.host
-                      + '/connect/' + provider.name + '/callback';
+    var flow = flows[provider.oauth]
 
     if (provider.oauth == 1) {
-      request.post(provider.request_url, {
-        oauth:{
-          callback:redirect_uri,
-          consumer_key:provider.key,
-          consumer_secret:provider.secret
-        }
-      }, function (err, _res, body) {
-        if (err) console.log(err);
-        var data = qs.parse(body);
-        req.session.payload = data;
+      flow.step1(provider, function (err, data) {
+        // TODO: handle errors
+        if (err) console.log(err)
 
-        var params = {
-          oauth_token:data.oauth_token
-        }
-        if (provider.flickr) {
-          params.perms = provider.scope;
-        }
-        if (provider.trello) {
-          params.scope = provider.scope;
-          params.expiration = provider.expiration;
-        }
+        req.session.payload = data
 
-        var url = provider.authorize_url + '?' + qs.stringify(params)
-        res.redirect(url);
-      });
+        var url = flow.step2(provider, data)
+        res.redirect(url)
+      })
     }
 
     else if (provider.oauth == 2) {
-      var params = {
-        client_id:provider.key,
-        response_type:'code',
-        redirect_uri:redirect_uri,
-        scope:provider.scope,
-        state:provider.state
-      }
-      if (provider.google) {
-        params.access_type = provider.access_type;
-      }
-      if (provider.reddit) {
-        params.duration = provider.duration;
-      }
-      res.redirect(provider.authorize_url + '?' + qs.stringify(params));
+      var url = flow.step1(provider)
+      res.redirect(url)
     }
 
     else if (provider.custom) {
-      if (provider.name == 'getpocket') {
-        request.post(provider.request_url, {
-          headers: {
-            // 'x-accept':'application/json'
-            'x-accept':'application/x-www-form-urlencoded'
-          },
-          form: {
-            consumer_key:provider.key,
-            redirect_uri:redirect_uri,
-            state:provider.state
-          }
-        }, function (err, _res, body) {
-          if (err) console.log(err)
-          var data = qs.parse(body);
-          req.session.payload = data;
+      flow = flows[provider.name]
+      flow.step1(provider, function (err, data) {
+        // TODO: handle errors
+        if (err) console.log(err)
 
-          var url = provider.authorize_url + '?' + qs.stringify({
-            request_token:data.code,
-            redirect_uri:redirect_uri
-          })
-          res.redirect(url);
-        });
-      }
+        req.session.payload = data
+
+        var url = flow.step2(provider, data)
+        res.redirect(url)
+      })
     }
   };
 
   app.get('/connect/:provider/callback', function (req, res) {
     var provider = app.config.app[req.session.provider];
 
+    var flow = flows[provider.oauth]
+
     if (provider.oauth == 1) {
       var data = req.session.payload;
       delete req.session.payload;
-      var options = {
-        oauth:{
-          consumer_key:provider.key,
-          consumer_secret:provider.secret,
-          token:req.query.oauth_token,
-          token_secret:data.oauth_token_secret,
-          verifier:req.query.oauth_verifier
-        }
-      }
-      if (provider.goodreads) {
-        delete options.oauth.verifier;
-      }
-      request.post(provider.access_url, options, function (err, _res, body) {
-        if (err) console.log(err);
-        res.redirect(provider.callback + '?' + toQuerystring(provider, body));
-      });
+
+      flow.step3(provider, data, req.query, function (err, url) {
+        // TODO: handle error
+        if (err) console.log(err)
+        res.redirect(url)
+      })
     }
     else if (provider.oauth == 2) {
-      var redirect_uri = provider.protocol + '://' + provider.host
-                      + '/connect/' + provider.name + '/callback';
-      var options = {
-        form:{
-          client_id:provider.key,
-          client_secret:provider.secret,
-          code:req.query.code,
-          redirect_uri:redirect_uri,
-          grant_type:'authorization_code'
-        }
-      }
-      if (provider.reddit) {
-        delete options.form.client_id;
-        delete options.form.client_secret;
-        options.auth = {user:provider.key, pass:provider.secret}
-      }
-      request.post(provider.access_url, options, function (err, _res, body) {
-        if (err) console.log(err);
-        res.redirect(provider.callback + '?' + toQuerystring(provider, body));
-      });
+      flow.step2(provider, req.query, function (err, data) {
+        // TODO: handle error
+        if (err) console.log(err)
+
+        var url = flow.step3(provider, data)
+        res.redirect(url)
+      })
     }
 
     else if (provider.custom) {
-      if (provider.name == 'getpocket') {
-        var data = req.session.payload;
-        delete req.session.payload;
-        request.post(provider.access_url, {
-          headers: {
-            // 'x-accept':'application/json'
-            'x-accept':'application/x-www-form-urlencoded'
-          },
-          form: {
-            consumer_key:provider.key,
-            code:data.code
-          }
-        }, function (err, _res, body) {
-          if (err) console.log(err);
-          res.redirect(provider.callback + '?' + toQuerystring(provider, body));
-        });
-      }
+      flow = flows[provider.name]
+
+      var data = req.session.payload
+      delete req.session.payload
+
+      flow.step3(provider, data, function (err, url) {
+        // TODO: handle error
+        if (err) console.log(err)
+        res.redirect(url)
+      })
     }
   });
-
-  function toQuerystring (provider, body) {
-    var data;
-    try {data = JSON.parse(body)} catch (e) {}
-    data = data || qs.parse(body);
-
-    var result = {};
-    if (provider.yammer) {
-      result.access_token = data.access_token.token;
-    }
-    else if (provider.oauth == 1) {
-      for (var key in data) {
-        if (key == 'oauth_token') {
-          result.access_token = data.oauth_token;
-        }
-        else if (key == 'oauth_token_secret') {
-          result.access_secret = data.oauth_token_secret;
-        }
-      }
-    }
-    else if (provider.oauth == 2) {
-      for (var key in data) {
-        if (key == 'access_token') {
-          result.access_token = data.access_token;
-        }
-        else if (key == 'refresh_token') {
-          result.refresh_token = data.refresh_token;
-        }
-      }
-    }
-    result.raw = data;
-
-    return qs.stringify(result);
-  }
 
   return app;
 }
