819fe9b1dfdc493a8592b6924721e2b12d0bbe81
simov <simeonvelichkov@gmail.com>
2014-12-23 14:02:43 +0200

    Minor refactoring in kue example app

--- example/koa.js
+++ example/koa.js
@@ -1,6 +1,7 @@
 
-var fs = require('fs')
-var path = require('path')
+var fs = require('fs'),
+  path = require('path')
+
 var koa = require('koa'),
   router = require('koa-router'),
   mount = require('koa-mount'),
@@ -8,28 +9,30 @@ var koa = require('koa'),
   session = require('koa-session'),
   favicon = require('koa-favicon'),
   accesslog = require('koa-accesslog')
-var qs = require('qs')
-var extend = require('extend')
-var hogan = require('hogan.js')
-var Grant = require('../index').koa()
-var template = hogan.compile(fs.readFileSync(path.join(__dirname, 'template.html'),'utf8'))
-var port = 3000
+
+var qs = require('qs'),
+  extend = require('extend'),
+  hogan = require('hogan.js'),
+  Grant = require('../index').koa()
+
+var template = hogan.compile(fs.readFileSync(path.join(__dirname, 'template.html'),'utf8')),
+  port = 3000
 
 
 var config = {
   server: require('./config/server.json'),
   credentials: require('./config/credentials.json'),
   options: require('./config/options.json')
-};
+}
 
 function transform (config) {
-  var result = {server: config.server};
+  var result = {server: config.server}
   for (var key in config.credentials) {
-    var provider = {};
-    extend(true, provider, config.credentials[key], config.options[key]);
-    result[key] = provider;
+    var provider = {}
+    extend(true, provider, config.credentials[key], config.options[key])
+    result[key] = provider
   }
-  return result;
+  return result
 }
 
 
@@ -49,27 +52,27 @@ app.get('/', function *(next) {
 
   // feedly sandbox redirect_uri
   if (session.provider == 'feedly' && this.query.code) {
-    var q = require('querystring');
-    this.response.redirect('/connect/feedly/callback?'+q.stringify(this.query));
-    return;
+    var q = require('querystring')
+    this.response.redirect('/connect/feedly/callback?'+q.stringify(this.query))
+    return
   }
 
-  console.log(this.query);
-  console.log(qs.parse(qs.stringify(this.query)));
+  console.log(this.query)
+  console.log(qs.parse(qs.stringify(this.query)))
   var $this = this
 
-  var providers = Object.keys(grant.config);
-  var params = [];
+  var providers = Object.keys(grant.config)
+  var params = []
 
   providers.forEach(function (provider) {
-    var obj = {url:'/connect/'+provider, name:provider};
+    var obj = {url:'/connect/'+provider, name:provider}
     if (session.provider == provider) {
-      obj.credentials = $this.query;
-      var key = $this.query.error ? 'error' : 'raw';
-      obj.credentials[key] = JSON.stringify($this.query[key], null, 4);
+      obj.credentials = $this.query
+      var key = $this.query.error ? 'error' : 'raw'
+      obj.credentials[key] = JSON.stringify($this.query[key], null, 4)
     }
-    params.push(obj);
-  });
+    params.push(obj)
+  })
   this.body = template.render({
     providers:params,
     count:providers.length-1//linkedin2
@@ -77,5 +80,5 @@ app.get('/', function *(next) {
 })
 
 app.listen(port, function() {
-  console.log('Koa server listening on port ' + port);
-});
+  console.log('Koa server listening on port ' + port)
+})
