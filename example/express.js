d199d6dc2b1f3e1acfb2ed9b18972074a9f3cb43
simov <simeonvelichkov@gmail.com>
2014-12-26 11:47:40 +0200

    Remove ; symbol in express example app

--- example/express.js
+++ example/express.js
@@ -4,30 +4,31 @@ var express = require('express'),
   bodyParser = require('body-parser'),
   cookieParser = require('cookie-parser'),
   session = require('express-session'),
-  favicon = require('serve-favicon');
+  favicon = require('serve-favicon')
+
 var consolidate = require('consolidate'),
-  hogan = require('hogan.js');
-var extend = require('extend');
-var Grant = require('../index').express()
+  hogan = require('hogan.js'),
+  extend = require('extend'),
+  Grant = require('../index').express()
 
 var config = {
   server: require('./config/server.json'),
   credentials: require('./config/credentials.json'),
   options: require('./config/options.json')
-};
+}
 
 function transform (config) {
-  var result = {server: config.server};
+  var result = {server: config.server}
   for (var key in config.credentials) {
-    var provider = {};
-    extend(true, provider, config.credentials[key], config.options[key]);
-    result[key] = provider;
+    var provider = {}
+    extend(true, provider, config.credentials[key], config.options[key])
+    result[key] = provider
   }
-  return result;
+  return result
 }
 
 
-var grant = new Grant(transform(config));
+var grant = new Grant(transform(config))
 
 
 var app = express()
@@ -40,24 +41,24 @@ var app = express()
 
   .use(function (req, res, next) {
     if (/^\/connect\/(\w+)$/.test(req.url)) {
-      var name = req.url.replace(/^\/connect\/(\w+)$/,'$1');
-      var provider = grant.config[name];
+      var name = req.url.replace(/^\/connect\/(\w+)$/,'$1')
+      var provider = grant.config[name]
 
       if (provider.protocol == 'https') {
         if (/^http:/.test(req.headers.referer)) {
-          var url = provider.protocol+'://'+provider.host+'/connect/'+provider.name;
-          return res.redirect(url);
+          var url = provider.protocol+'://'+provider.host+'/connect/'+provider.name
+          return res.redirect(url)
         }
       }
       else {
         if (/^https:/.test(req.headers.referer)) {
-          var url = provider.protocol+'://'+provider.host+'/connect/'+provider.name;
-          return res.redirect(url);
+          var url = provider.protocol+'://'+provider.host+'/connect/'+provider.name
+          return res.redirect(url)
         }
       }
     }
 
-    next();
+    next()
   })
 
   .use(grant)
@@ -70,7 +71,7 @@ var app = express()
 
   .use(logger('dev'))
   .use(bodyParser.json())
-  .use(bodyParser.urlencoded({extended: true}));
+  .use(bodyParser.urlencoded({extended: true}))
 
 
 app.get('/', function (req, res) {
@@ -78,31 +79,31 @@ app.get('/', function (req, res) {
 
   // feedly sandbox redirect_uri
   if (session.provider == 'feedly' && req.query.code) {
-    var q = require('querystring');
-    res.redirect('/connect/feedly/callback?'+q.stringify(req.query));
-    return;
+    var q = require('querystring')
+    res.redirect('/connect/feedly/callback?'+q.stringify(req.query))
+    return
   }
 
-  console.log(req.query);
+  console.log(req.query)
 
-  var providers = Object.keys(grant.config);
-  var params = [];
+  var providers = Object.keys(grant.config)
+  var params = []
 
   providers.forEach(function (provider) {
-    var obj = {url:'/connect/'+provider, name:provider};
+    var obj = {url:'/connect/'+provider, name:provider}
     if (session.provider == provider) {
-      obj.credentials = req.query;
-      var key = req.query.error ? 'error' : 'raw';
-      obj.credentials[key] = JSON.stringify(req.query[key], null, 4);
+      obj.credentials = req.query
+      var key = req.query.error ? 'error' : 'raw'
+      obj.credentials[key] = JSON.stringify(req.query[key], null, 4)
     }
-    params.push(obj);
-  });
+    params.push(obj)
+  })
   res.render('template', {
     providers:params,
     count:providers.length-1//linkedin2
-  });
-});
+  })
+})
 
 app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+  console.log('Express server listening on port ' + app.get('port'))
+})
