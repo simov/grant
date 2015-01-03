7d70ccc7ac7206ad7048f0d1585ee59ed1a7bfcb
simov <simeonvelichkov@gmail.com>
2015-01-03 11:44:46 +0200

    Add feedly sandbox urls to the example app

--- example/express.js
+++ example/express.js
@@ -48,13 +48,17 @@ app.set('view cache', true)
 app.engine('html', consolidate.hogan)
 
 
+// evernote sandbox urls
+grant.config.evernote.request_url = grant.config.evernote.request_url.replace('www','sandbox')
+grant.config.evernote.authorize_url = grant.config.evernote.authorize_url.replace('www','sandbox')
+grant.config.evernote.access_url = grant.config.evernote.access_url.replace('www','sandbox')
+// feedly sandbox urls
+grant.config.feedly.authorize_url = grant.config.feedly.authorize_url.replace('cloud','sandbox')
+grant.config.feedly.access_url = grant.config.feedly.access_url.replace('cloud','sandbox')
+
+
 app.get('/', function (req, res) {
   var session = req.session.grant||{}
-
-  // evernote sandbox urls
-  grant.config.evernote.request_url = grant.config.evernote.request_url.replace('www','sandbox')
-  grant.config.evernote.authorize_url = grant.config.evernote.authorize_url.replace('www','sandbox')
-  grant.config.evernote.access_url = grant.config.evernote.access_url.replace('www','sandbox')
 
   // feedly sandbox redirect_uri
   if (session.provider == 'feedly' && req.query.code) {
