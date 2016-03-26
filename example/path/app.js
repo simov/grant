0f7ae58df0c69bd78659ce27a1740aaded692fcf
simov <simeonvelichkov@gmail.com>
2016-03-26 13:17:31 +0200

    Add path prefix example

---
+++ example/path/app.js
@@ -0,0 +1,23 @@
+
+var express = require('express')
+var logger = require('morgan')
+var session = require('express-session')
+
+var Grant = require('grant-express')
+var grant = new Grant(require('./config.json'))
+
+var app = express()
+app.use(logger('dev'))
+// REQUIRED:
+app.use(session({secret: 'very secret'}))
+// mount grant
+app.use('/path/prefix', grant)
+
+app.get('/handle_twitter_callback', function (req, res) {
+  console.log(req.query)
+  res.end(JSON.stringify(req.query, null, 2))
+})
+
+app.listen(3000, function () {
+  console.log('Express server listening on port ' + 3000)
+})
