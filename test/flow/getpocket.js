8423e33acfa72d63775e600a0c2db17876355e76
simov <simeonvelichkov@gmail.com>
2016-04-08 11:07:25 +0300

    Refactor getpocket flow and tests

--- test/flow/getpocket.js
+++ test/flow/getpocket.js
@@ -9,49 +9,59 @@ var getpocket = require('../../lib/flow/getpocket')
 
 describe('getpocket', function () {
   function url (path) {
-    var c = config.server
-    return c.protocol + '://' + c.host + path
+    return 'http://localhost:5000' + path
   }
 
-  var grant, app, server
-  var config = {
-    server: {protocol: 'http', host: 'localhost:5000', callback: '/'},
-    getpocket: {}
-  }
+  var grant, server
 
   before(function (done) {
+    var config = {
+      getpocket: {
+        request_url: url('/request_url'),
+        authorize_url: '/authorize_url',
+        access_url: url('/access_url'),
+        redirect_uri: '/redirect_uri',
+        key: 'key',
+        state: '123'
+      }
+    }
     grant = new Grant(config)
-    app = express().use(grant)
-
-    grant.config.getpocket.request_url = url('/request_url')
-    grant.config.getpocket.authorize_url = '/authorize_url'
-    grant.config.getpocket.access_url = url('/access_url')
+    var app = express().use(grant)
 
     app.post('/request_url', function (req, res) {
-      res.end(qs.stringify({some: 'data'}))
+      req.pipe(res)
     })
     app.post('/access_url', function (req, res) {
-      res.end(JSON.stringify({some: 'data'}))
+      res.end(qs.stringify({access_token: 'token', some: 'data'}))
     })
     server = app.listen(5000, done)
   })
 
   it('step1', function (done) {
     getpocket.step1(grant.config.getpocket, function (err, data) {
-      t.deepEqual(data, {some: 'data'})
+      t.deepEqual(data, {
+        consumer_key: 'key',
+        redirect_uri: '/redirect_uri',
+        state: '123'
+      })
       done()
     })
   })
 
   it('step2', function () {
     var url = getpocket.step2(grant.config.getpocket, {code: 'code'})
-    t.equal(url, '/authorize_url?request_token=code' +
-      '&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fconnect%2Fgetpocket%2Fcallback')
+    t.deepEqual(qs.parse(url.replace('/authorize_url?', '')), {
+      request_token: 'code',
+      redirect_uri: '/redirect_uri'
+    })
   })
 
   it('step3', function (done) {
     getpocket.step3(grant.config.getpocket, {}, function (err, url) {
-      t.equal(url, 'raw%5Bsome%5D=data')
+      t.deepEqual(qs.parse(url), {
+        access_token: 'token',
+        raw: {access_token: 'token', some: 'data'}
+      })
       done()
     })
   })
