c3151b49b80bfc8bc3a90980bbfbb9bcd7a76483
simov <simeonvelichkov@gmail.com>
2018-12-07 11:04:57 +0200

    Update build script as npx is not available in node < 8

--- build.sh
+++ build.sh
@@ -8,8 +8,11 @@
 # prepublish
 rm -rf build/ && mkdir build
 
+# alias
+babel=node_modules/.bin/babel
+
 # transpile
-npx babel lib/consumer --out-dir build/lib/consumer
+$babel lib/consumer --out-dir build/lib/consumer
 # do not transpile
 # hapi17 and koa2 depend on Node >= 8
 # koa1 works natively on Node >= 4
@@ -18,10 +21,10 @@ cp lib/consumer/koa2.js build/lib/consumer/
 cp lib/consumer/hapi17.js build/lib/consumer/
 
 # transpile
-npx babel lib/flow --out-dir build/lib/flow
-npx babel lib/*.js --out-dir build
-npx babel test --out-dir build/test
-npx babel *.js --out-dir build
+$babel lib/flow --out-dir build/lib/flow
+$babel lib/*.js --out-dir build
+$babel test --out-dir build/test
+$babel *.js --out-dir build
 
 # copy
 cp -r config/ build/
