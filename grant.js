7d4a4c3bd2aa87f4f0504f31ff8854355e76b4e4
simov <simeonvelichkov@gmail.com>
2020-03-02 20:15:34 +0200

    Add new constructor

--- grant.js
+++ grant.js
@@ -1,14 +1,20 @@
 
-exports.express = () => {
+function grant ({config, handler}) {
+  return require(`./lib/consumer/${handler}`)(config)
+}
+
+// DEPRECATED
+
+grant.express = () => {
   return require('./lib/consumer/express')
 }
 
-exports.koa = () => {
+grant.koa = () => {
   var version = parseInt(require('koa/package.json').version.split('.')[0])
   return require('./lib/consumer/koa' + (version < 2 ? '' : '2'))
 }
 
-exports.hapi = () => {
+grant.hapi = () => {
   var pkg
   try {
     pkg = require('hapi/package.json')
@@ -19,3 +25,5 @@ exports.hapi = () => {
   var version = parseInt(pkg.version.split('.')[0])
   return require('./lib/consumer/hapi' + (version < 17 ? '' : '17'))
 }
+
+module.exports = grant
