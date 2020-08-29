be4ee92c4330d41e2e900e9a250c8a2a8b2c22bb
simov <simeonvelichkov@gmail.com>
2020-08-29 07:48:48 +0300

    Add fastify examples

---
+++ examples/dynamic-state/fastify.js
@@ -0,0 +1,27 @@
+
+var fastify = require('fastify')
+var cookie = require('fastify-cookie')
+var session = require('fastify-session')
+var grant = require('../../').fastify()
+
+
+fastify()
+  .decorateRequest('grant', {})
+  .addHook('preHandler', async (req, res) => {
+    if (/^\/connect\/google/.test(req.url)) {
+      req.grant = {dynamic: {scope: ['openid']}}
+    }
+    else if (/^\/connect\/twitter/.test(req.url)) {
+      req.grant = {dynamic: {key: 'CONSUMER_KEY', 'secret': 'CONSUMER_SECRET'}}
+    }
+  })
+  .register(cookie)
+  .register(session, {secret: '01234567890123456789012345678912', cookie: {secure: false}})
+  .register(grant(require('./config')))
+  .route({method: 'GET', path: '/hello', handler: async (req, res) => {
+    res.send(JSON.stringify(req.session.grant.response, null, 2))
+  }})
+  .route({method: 'GET', path: '/hi', handler: async (req, res) => {
+    res.send(JSON.stringify(req.session.grant.response, null, 2))
+  }})
+  .listen(3000)
---
+++ examples/transport-state/fastify.js
@@ -0,0 +1,21 @@
+
+var fastify = require('fastify')
+var cookie = require('fastify-cookie')
+var session = require('fastify-session')
+var grant = require('../../').fastify()
+
+
+fastify()
+  .decorateReply('grant', {})
+  .addHook('onSend', async (req, res, payload) => {
+    if (/^\/connect\/.*?\/callback/.test(req.url)) {
+      res.header('content-type', 'text/plain')
+      payload = JSON.stringify(res.grant.response, null, 2)
+      return payload
+    }
+    return payload
+  })
+  .register(cookie)
+  .register(session, {secret: '01234567890123456789012345678912', cookie: {secure: false}})
+  .register(grant(require('./config')))
+  .listen(3000)
