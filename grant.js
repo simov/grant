5cf3c80e827b09fde2099b1b48dfffa1d23f4476
simov <simeonvelichkov@gmail.com>
2021-03-20 16:29:23 +0200

    Update imports

--- grant.js
+++ grant.js
@@ -1,33 +1,68 @@
 
 function grant ({handler, ...rest}) {
   if (handler === 'express') {
-    var version = 4
     return require('./lib/handler/express-4')(rest)
   }
   else if (handler === 'koa') {
-    if (parseInt(require('koa/package.json').version.split('.')[0]) >= 2) {
-      return require('./lib/handler/koa-2')(rest)
+    try {
+      var pkg = require('koa/package.json')
     }
-    else {
-      return require('./lib/handler/koa-1')(rest)
-    }
-  
+    catch (err) {}
+    var version = pkg ? parseInt(pkg.version.split('.')[0]) : 2
+    return version >= 2
+      ? require('./lib/handler/koa-2')(rest)
+      : require('./lib/handler/koa-1')(rest)
   }
   else if (handler === 'hapi') {
     try {
       var pkg = require('@hapi/hapi/package.json')
     }
     catch (err) {
-      var pkg = require('hapi/package.json')
+      try {
+        var pkg = require('hapi/package.json')
+      }
+      catch (err) {}
     }
-    var version = parseInt(pkg.version.split('.')[0]) >= 17 ? 17 : 16
-    if (version >= 17) {
-      return require('./lib/handler/hapi-17')(rest)
-    }
-    else {
-      return require('./lib/handler/hapi-16')(rest)
-    }
-
+    var version = pkg ? parseInt(pkg.version.split('.')[0]) : 17
+    return version >= 17
+      ? require('./lib/handler/hapi-17')(rest)
+      : require('./lib/handler/hapi-16')(rest)
+  }
+  else if (handler === 'express-4') {
+    return require('./lib/handler/express-4')(rest)
+  }
+  else if (handler === 'koa-2') {
+    return require('./lib/handler/koa-2')(rest)
+  }
+  else if (handler === 'koa-1') {
+    return require('./lib/handler/koa-1')(rest)
+  }
+  else if (handler === 'hapi-17') {
+    return require('./lib/handler/hapi-17')(rest)
+  }
+  else if (handler === 'hapi-16') {
+    return require('./lib/handler/hapi-16')(rest)
+  }
+  else if (handler === 'fastify') {
+    return require('./lib/handler/fastify')(rest)
+  }
+  else if (handler === 'curveball') {
+    return require('./lib/handler/curveball')(rest)
+  }
+  else if (handler === 'node') {
+    return require('./lib/handler/node')(rest)
+  }
+  else if (handler === 'aws') {
+    return require('./lib/handler/aws')(rest)
+  }
+  else if (handler === 'azure') {
+    return require('./lib/handler/azure')(rest)
+  }
+  else if (handler === 'gcloud') {
+    return require('./lib/handler/gcloud')(rest)
+  }
+  else if (handler === 'vercel') {
+    return require('./lib/handler/vercel')(rest)
   }
 }
 
@@ -37,14 +72,14 @@ grant.express = (options) => {
 }
 
 grant.koa = (options) => {
-  var version =
-    parseInt(require('koa/package.json').version.split('.')[0]) >= 2 ? 2 : 1
-  if (version >= 2) {
-    var handler = require('./lib/handler/koa-2')
+  try {
+    var pkg = require('koa/package.json')
   }
-  else {
-    var handler = require('./lib/handler/koa-1')
-  }
+  catch (err) {}
+  var version = pkg ? parseInt(pkg.version.split('.')[0]) : 2
+  var handler = version >= 2
+    ? require('./lib/handler/koa-2')
+    : require('./lib/handler/koa-1')
   return options ? handler(options) : handler
 }
 
@@ -53,43 +88,49 @@ grant.hapi = (options) => {
     var pkg = require('@hapi/hapi/package.json')
   }
   catch (err) {
-    var pkg = require('hapi/package.json')
+    try {
+      var pkg = require('hapi/package.json')
+    }
+    catch (err) {}
   }
-  var version = parseInt(pkg.version.split('.')[0]) >= 17 ? 17 : 16
-  if (version >= 17) {
-    var handler = require('./lib/handler/hapi-17')(rest)
-  }
-  else {
-    var handler = require('./lib/handler/hapi-16')(rest)
-  }
+  var version = pkg ? parseInt(pkg.version.split('.')[0]) : 17
+  var handler = version >= 17
+    ? require('./lib/handler/hapi-17')
+    : require('./lib/handler/hapi-16')
   return options ? handler(options) : handler
 }
 
-grant['fastify'] = (options) => {
+grant.fastify = (options) => {
   var handler = require('./lib/handler/fastify')
   return options ? handler(options) : handler
 }
-grant['curveball'] = (options) => {
+
+grant.curveball = (options) => {
   var handler = require('./lib/handler/curveball')
   return options ? handler(options) : handler
 }
-grant['node'] = (options) => {
+
+grant.node = (options) => {
   var handler = require('./lib/handler/node')
   return options ? handler(options) : handler
 }
-grant['aws'] = (options) => {
+
+grant.aws = (options) => {
   var handler = require('./lib/handler/aws')
   return options ? handler(options) : handler
 }
-grant['azure'] = (options) => {
+
+grant.azure = (options) => {
   var handler = require('./lib/handler/azure')
   return options ? handler(options) : handler
 }
-grant['gcloud'] = (options) => {
+
+grant.gcloud = (options) => {
   var handler = require('./lib/handler/gcloud')
   return options ? handler(options) : handler
 }
-grant['vercel'] = (options) => {
+
+grant.vercel = (options) => {
   var handler = require('./lib/handler/vercel')
   return options ? handler(options) : handler
 }
