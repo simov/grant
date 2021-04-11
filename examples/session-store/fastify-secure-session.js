26dce5be9afb392ace30573e8f28d41a9b571441
Marcus Spiegel <spiegel@uscreen.de>
2021-04-11 19:37:05 +0200

    added example fastify-secure-session

---
+++ examples/session-store/fastify-secure-session.js
@@ -0,0 +1,27 @@
+
+var fastify = require('fastify')
+var session = require('fastify-secure-session')
+var grant = require('../../').fastify()
+
+
+fastify()
+  .register(session, {
+    secret: '01234567890123456789012345678912',
+    salt: 'mOXbPOPAcEb8hMDH',
+    cookie: { path: '/', httpOnly: true }
+  })
+  .addHook('onRequest', (req, res, next) => {
+    Object.defineProperty(req.session, 'grant', {
+      get: () => req.session.get('grant'),
+      set: (value) => req.session.set('grant', value)
+    })
+    next()
+  })
+  .register(grant(require('./config')))
+  .route({method: 'GET', path: '/hello', handler: async (req, res) => {
+    res.send(JSON.stringify(req.session.grant.response, null, 2))
+  }})
+  .route({method: 'GET', path: '/hi', handler: async (req, res) => {
+    res.send(JSON.stringify(req.session.grant.response, null, 2))
+  }})
+  .listen(3000)
